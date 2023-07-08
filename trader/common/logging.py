b19a4e518abfa739f04c012f48663c107b61a233
HLIB SIN <sinixyowo@gmail.com>
2023-07-08 15:44:36 +0300

    1.2.1pre1

--- trader/common/logging.py
+++ trader/common/logging.py
@@ -8,7 +8,7 @@ import logging
 
 logger = Logger(name='PULLBACK', level=LogLevel.INFO)
 handler = AsyncFileHandler(filename='logs.txt')
-handler.formatter = Formatter('[%(levelname)s] [%(name)s] %(asctime)s :: %(message)s', '%Y-%m-%d_%H:%M:%S')
+handler.formatter = Formatter('[%(levelname)s] [%(name)s] %(asctime)s.%(msecs)03d :: %(message)s', '%Y-%m-%d %H:%M:%S')
 logger.add_handler(handler)
 
 logging.basicConfig(
